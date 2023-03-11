97d048bacc6b26ddf38855a7a73a89358eacde0d
Theanh0411 <theanh@mogul.sg>
2023-03-11 22:04:41 +0700

    fix: ignore eslint in build mode

--- next.config.js
+++ next.config.js
@@ -13,7 +13,9 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: ['restaurant-management-0411.s3.amazonaws.com'],
   },
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
